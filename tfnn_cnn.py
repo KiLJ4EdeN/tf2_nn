3a44f7e9fe2c24eaa1d7a00d6b0348de0bc87fb7
Abdolkarim Saeedi <parsa592323@gmail.com>
2021-11-19 18:54:17 +0330

    change the example to mnist

--- tfnn_cnn.py
+++ tfnn_cnn.py
@@ -1,76 +1,49 @@
-# define imports
-
+# imports and base params
+import itertools
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
-import itertools
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from tensorflow.keras.datasets import mnist
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report, accuracy_score
 
-# dummy data
 
-data = np.random.rand(100, 112, 112, 1)
-labels = np.random.randint(0, 4, size=(100))
 
-print(data.shape)
-print(labels.shape)
-
-X_train, X_test, Y_train, Y_test = train_test_split(data, 
-                                                    labels,
-                                                    stratify=labels,
-                                                    random_state=42,
-                                                    shuffle=True)
-print(X_train.shape)
-print(X_test.shape)
-
-encode = LabelEncoder()
-onehotencoder = OneHotEncoder()
-
-Y_train = encode.fit_transform(Y_train)
-train_labels = Y_train.reshape(-1,1)
-train_labels
-
-Y_test = encode.transform(Y_test)
-test_labels = Y_test.reshape(-1,1)
-
-Y_train = onehotencoder.fit_transform(train_labels)
-Y_train = Y_train.toarray()
-
-Y_test = onehotencoder.transform(test_labels)
-Y_test = Y_test.toarray()
-
-# Convert to float32.
-X_train = tf.cast(tf.constant(X_train), dtype=tf.float32)
-X_test = tf.cast(tf.constant(X_test), dtype=tf.float32)
-Y_train = tf.cast(tf.constant(Y_train), dtype=tf.float32)
-Y_test = tf.cast(tf.constant(Y_test), dtype=tf.float32)
-
-# tf params
-
-# number of classes
-num_classes = 4
+# MNIST dataset parameters.
+num_classes = 10 # total classes (0-9 digits).
 
 # Training parameters.
 learning_rate = 0.001
-training_steps = 200
-batch_size = 256
+batch_size = 128
 display_step = 10
 
 # Network parameters.
 conv1_filters = 32 # number of filters for 1st conv layer.
 conv2_filters = 64 # number of filters for 2nd conv layer.
-conv3_filters = 128 # number of filters for 3rd conv layer.
-conv4_filters = 256 # number of filters for 4th conv layer.
 fc1_units = 256 # number of neurons for 1st fully-connected layer.
+
+# Prepare MNIST data.
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+# Convert to float32.
+X_train = tf.cast(tf.constant(X_train), dtype=tf.float32)
+X_test = tf.cast(tf.constant(X_test), dtype=tf.float32)
+
+# Normalize images value from [0, 255] to [0, 1].
+X_train, X_test = X_train / 255., X_test / 255.
+
+print(X_train.shape)
+print(X_test.shape)
 
 # Use tf.data API to shuffle and batch data.
 train_data = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
-test_data = tf.data.Dataset.from_tensor_slices((X_test, Y_test))
 train_data = train_data.shuffle(5000).batch(batch_size).prefetch(1)
+
+# test
+test_data = tf.data.Dataset.from_tensor_slices((X_test, Y_test))
 test_data = test_data.shuffle(5000).batch(batch_size).prefetch(1)
 
+# MODEL
 # Create some wrappers for simplicity.
 def conv2d(x, W, b, strides=1):
     # Conv2D wrapper, with bias and relu activation.
@@ -93,12 +66,8 @@ weights = {
     'wc1': tf.Variable(random_normal([5, 5, 1, conv1_filters])),
     # Conv Layer 2: 5x5 conv, 32 inputs, 64 filters.
     'wc2': tf.Variable(random_normal([5, 5, conv1_filters, conv2_filters])),
-    # Conv Layer 3: 5x5 conv, 32 inputs, 64 filters.
-    'wc3': tf.Variable(random_normal([5, 5, conv2_filters, conv3_filters])),
-    # Conv Layer 4: 5x5 conv, 32 inputs, 64 filters.
-    'wc4': tf.Variable(random_normal([5, 5, conv3_filters, conv4_filters])),
     # FC Layer 1: 7*7*64 inputs, 1024 units.
-    'wd1': tf.Variable(random_normal([7*7*256, fc1_units])),
+    'wd1': tf.Variable(random_normal([7*7*64, fc1_units])),
     # FC Out Layer: 1024 inputs, 10 units (total number of classes)
     'wout': tf.Variable(random_normal([fc1_units, num_classes]))
 }
@@ -106,8 +75,6 @@ weights = {
 biases = {
     'bc1': tf.Variable(tf.zeros([conv1_filters])),
     'bc2': tf.Variable(tf.zeros([conv2_filters])),
-    'bc3': tf.Variable(tf.zeros([conv3_filters])),
-    'bc4': tf.Variable(tf.zeros([conv4_filters])),
     'bd1': tf.Variable(tf.zeros([fc1_units])),
     'bout': tf.Variable(tf.zeros([num_classes]))
 }
@@ -115,46 +82,40 @@ biases = {
 # Create model
 def conv_net(x):
     
-    # Input shape: [-1, 112, 112, 1]. A batch of 112x112x1 (grayscale) images.
-    x = tf.reshape(x, [-1, 112, 112, 1])
+    # Input shape: [-1, 28, 28, 1]. A batch of 28x28x1 (grayscale) images.
+    x = tf.reshape(x, [-1, 28, 28, 1])
 
-    # Convolution Layer. Output shape: [-1, 112, 112, 32].
+    # Convolution Layer. Output shape: [-1, 28, 28, 32].
     conv1 = conv2d(x, weights['wc1'], biases['bc1'])
-    # Max Pooling (down-sampling). Output shape: [-1, 56, 56, 32].
+    
+    # Max Pooling (down-sampling). Output shape: [-1, 14, 14, 32].
     conv1 = maxpool2d(conv1, k=2)
 
-    # Convolution Layer. Output shape: [-1, 56, 56, 64].
+    # Convolution Layer. Output shape: [-1, 14, 14, 64].
     conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
-    # Max Pooling (down-sampling). Output shape: [-1, 28, 28, 64].
+    
+    # Max Pooling (down-sampling). Output shape: [-1, 7, 7, 64].
     conv2 = maxpool2d(conv2, k=2)
 
-    # Convolution Layer. Output shape: [-1, 28, 28, 128].
-    conv3 = conv2d(conv2, weights['wc3'], biases['bc3'])
-    # Max Pooling (down-sampling). Output shape: [-1, 14, 14, 128].
-    conv3 = maxpool2d(conv3, k=2)
-
-    # Convolution Layer. Output shape: [-1, 14, 14, 256].
-    conv4 = conv2d(conv3, weights['wc4'], biases['bc4'])
-    # Max Pooling (down-sampling). Output shape: [-1, 7, 7, 256].
-    conv4 = maxpool2d(conv4, k=2)
-
-    # Reshape conv2 output to fit fully connected layer input, Output shape: [-1, 7*7*256].
-    fc1 = tf.reshape(conv4, [-1, weights['wd1'].get_shape().as_list()[0]])
+    # Reshape conv2 output to fit fully connected layer input, Output shape: [-1, 7*7*64].
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
     
-    # Fully connected layer, Output shape: [-1, 256].
+    # Fully connected layer, Output shape: [-1, 1024].
     fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
     # Apply ReLU to fc1 output for non-linearity.
     fc1 = tf.nn.relu(fc1)
 
-    # Fully connected layer, Output shape: [-1, 4].
+    # Fully connected layer, Output shape: [-1, 10].
     out = tf.add(tf.matmul(fc1, weights['wout']), biases['bout'])
     # Apply softmax to normalize the logits to a probability distribution.
     return tf.nn.softmax(out)
 
+# METRICS
+
 # Cross-Entropy loss function.
 def cross_entropy(y_pred, y_true):
-    # already one hot encoded
-    # y_true = tf.one_hot(y_true, depth=num_classes)
+    # Encode label to a one hot vector.
+    y_true = tf.one_hot(y_true, depth=num_classes)
     # Clip prediction values to avoid log(0) error.
     y_pred = tf.clip_by_value(y_pred, 1e-9, 1.)
     # Compute cross-entropy.
@@ -163,13 +124,13 @@ def cross_entropy(y_pred, y_true):
 # Accuracy metric.
 def accuracy(y_pred, y_true):
     # Predicted class is the index of highest score in prediction vector (i.e. argmax).
-    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(tf.cast(y_true, tf.int64), 1))
+    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.cast(y_true, tf.int64))
     return tf.reduce_mean(tf.cast(correct_prediction, tf.float32), axis=-1)
 
 # ADAM optimizer.
 optimizer = tf.optimizers.Adam(learning_rate)
 
-# Optimization process. 
+# training function
 def run_train(x, y):
     # Wrap computation inside a GradientTape for automatic differentiation.
     with tf.GradientTape() as g:
@@ -185,11 +146,11 @@ def run_train(x, y):
     # Update W and b following gradients.
     optimizer.apply_gradients(zip(gradients, trainable_variables))
 
-# Calculate Metrics for the test set
+# testing function
 def run_test(test_loader):
   total_acc = 0
   total_loss = 0
-  for step, (batch_x, batch_y) in enumerate(test_loader.take(training_steps), 1):
+  for step, (batch_x, batch_y) in enumerate(test_loader):
     # predict using the model on test data
     pred = conv_net(batch_x)
     # calc acc and loss
@@ -197,7 +158,7 @@ def run_test(test_loader):
     acc = accuracy(pred, batch_y)
     total_acc += acc
     # keras way of dealing with loss
-    total_loss += loss / batch_size
+    total_loss += loss / len(batch_y)
   # calc total loss and acc
   total_acc = total_acc / (step + 1)
   total_loss = total_loss / (step + 1)
@@ -218,7 +179,7 @@ for epoch in range(epochs):
           pred = conv_net(batch_x)
           loss = cross_entropy(pred, batch_y)
           acc = accuracy(pred, batch_y)
-          print("TRAIN: step: %i, loss: %f, accuracy: %f" % (step + 1, loss / batch_size, acc))
+          print("TRAIN: step: %i, loss: %f, accuracy: %f" % (step + 1, loss / len(batch_y), acc))
 
   test_loss, test_acc = run_test(test_data)
   print("TEST: epoch: %i, loss: %f, accuracy: %f" % (epoch + 1, test_loss, test_acc))
@@ -249,7 +210,7 @@ plot_history(epoch_loss, epoch_acc)
 def evaluate(test_loader):
   total_acc = 0
   total_loss = 0
-  for step, (batch_x, batch_y) in enumerate(test_loader.take(training_steps)):
+  for step, (batch_x, batch_y) in enumerate(test_loader):
     # predict using the model on test data
     pred = conv_net(batch_x)
     # concate the shuffled labels and results
@@ -271,19 +232,18 @@ def evaluate(test_loader):
 
 loss, acc, pred, pred_t = evaluate(test_data)
 
-rep = classification_report(np.argmax(pred_t, axis=1), np.argmax(pred, axis=1))
-acc = accuracy_score(np.argmax(pred_t, axis=1), np.argmax(pred, axis=1))
+rep = classification_report(pred_t, np.argmax(pred, axis=1))
+acc = accuracy_score(pred_t, np.argmax(pred, axis=1))
 print(rep)
 print(acc)
 
 # confusion
-classes = ['0','1','2','3']
-cm = confusion_matrix(np.argmax(pred_t, axis=1), np.argmax(pred, axis=1))
+classes = ['0', '1','2','3', '4', '5', '6', '7', '8', '9']
+cm = confusion_matrix(pred_t, np.argmax(pred, axis=1))
 plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
 plt.title('Confusion matrix')
 plt.colorbar()
-tick_marks = np.arange(4)
-
+tick_marks = np.arange(len(classes))
 plt.xticks(tick_marks, classes, rotation=45)
 plt.yticks(tick_marks, classes)
 thresh = cm.max() / 2.
